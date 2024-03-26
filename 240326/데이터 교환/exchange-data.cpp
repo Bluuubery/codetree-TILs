5dd21acc089a85e4384b776adec4da0dbe5a5c1c
Sun <zephyr72836@gmail.com>
2024-03-26 21:43:36 +0900

    Title: 데이터 교환, Time: 2ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5; int b = 6; int c = 7;
+    int temp1; int temp2;
+
+    temp1 = b; b = a; temp2= c; c = temp1; a = temp2;
+
+    cout << a << endl << b << endl << c;
+
+    return 0;
+}
