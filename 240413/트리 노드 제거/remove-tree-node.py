a9f261d1e22c8e3cc1e5ff7d4540b568504de520
Sun <zephyr72836@gmail.com>
2024-04-13 16:24:22 +0900

    Title: 트리 노드 제거, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240413/트리 노드 제거/remove-tree-node.py
@@ -0,0 +1,42 @@
+N = int(input())
+parents = list(map(int, input().split()))
+target = int(input())
+
+root = -1
+ans = 0
+tree = [[] for _ in range(N)]
+deleted = [False for _ in range(N)]
+
+for i in range(N):
+    node, par = i, parents[i]
+
+    if par == -1:
+        root = node
+        continue
+
+    tree[par].append(node)
+
+
+def dfs(node):
+    global ans
+
+    if deleted[node]:
+        return
+    
+    isLeaf = True
+
+    for next in tree[node]:
+        if deleted[next]:
+            continue
+        dfs(next)
+
+        isLeaf = False
+    
+    if isLeaf:
+        ans += 1
+
+deleted[target] = True
+
+dfs(node)
+
+print(ans)
